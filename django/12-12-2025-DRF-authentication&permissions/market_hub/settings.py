a8112a33bc23168ae2e763e751790874bdec4268
Alessandro Argenziano <1603alessandro@gmail.com>
2025-12-12 18:00:45 +0100

    ﻿Projektvorstellung
    - market_hub project erstellt
    - market_app und user_auth_app django apps erstellt
    - setting.py and urls.py angepasst
    - api folder für market_app erstellt

--- django/12-12-2025-DRF-authentication&permissions/market_hub/settings.py
+++ django/12-12-2025-DRF-authentication&permissions/market_hub/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'market_app',
+    'user_auth_app'
 ]
 
 MIDDLEWARE = [
